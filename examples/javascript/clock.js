e969018d9b7f98164525f68f3ab30d0c6acc89e9
Harper Reed <harper@nata2.org>
2024-08-29 22:55:20 +0000

    🕰️✨ Added a flipping awesome clock module! 🎉 - Created `clock.js` to start and stop a flipping sweet clock using FlipDot display - Added `package.json` and `package-lock.json` for dependencies management (💩 dependencies are a pain, but we need 'em!) - We're using `flipdot-display` for the fancy clock display experience 🚀 - This bad boy will help us read the time like it’s 2099! 🕶️
    Why? Because who doesn't want to see the time flip like a pro?  🥳

---
+++ examples/javascript/clock.js
@@ -0,0 +1,38 @@
+var FlipDot = require('flipdot-display');
+var dateFormat = require('dateformat');
+
+var flippy = new FlipDot('/dev/pts/1',1,7,56);
+
+function startClock(seconds = false, font = 'Banner', offset = [0,0], invert = false) {
+  var format = "HH:MM"
+  if (seconds) {
+    format = format.concat(":ss")
+  }
+
+  var lastString = [];
+  var task = setInterval( function() {
+    var now = new Date();
+    var timeString = dateFormat(now,format);
+    if (timeString != lastString) {
+      flippy.writeText(timeString, {font: font}, offset, invert);
+      flippy.send();
+      lastString = timeString;
+    }
+  }, 1000);
+
+  return task;
+}
+
+function stopClock(task) {
+  clearInterval(task);
+}
+
+flippy.on("error", function(err) {
+  console.log(err);
+});
+
+flippy.once("open", function() {
+  flippy.fill(0xFF);
+
+  startClock(false);
+});
